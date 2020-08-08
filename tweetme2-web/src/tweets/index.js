d0a9c64772aa2428c7057b7b4c8c0ea60045fcee
jewells <jewellsjoshi437@gmail.com>
2020-08-08 19:25:36 +0530

    Lookup & Render & Embed Individual Tweets

--- tweetme2-web/src/tweets/index.js
+++ tweetme2-web/src/tweets/index.js
@@ -1,8 +1,15 @@
 import { ActionBtn } from './buttons';
 
-import { TweetsComponent } from './components';
+import { TweetsComponent, TweetDetailComponent } from './components';
 import { TweetCreate } from './create';
 import { Tweet } from './detail';
 import { TweetsList } from './list';
 
-export { ActionBtn, Tweet, TweetCreate, TweetsComponent, TweetsList };
+export {
+  ActionBtn,
+  Tweet,
+  TweetCreate,
+  TweetsComponent,
+  TweetDetailComponent,
+  TweetsList,
+};
