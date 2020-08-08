3cb5dc644a1ff1fc7f1bc16dc956d2a1cf21e05e
jewells <jewellsjoshi437@gmail.com>
2020-08-08 17:54:15 +0530

    Rendering the ReTweet

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -109,6 +109,17 @@ export function ActionBtn(props) {
   );
 }
 
+export function ParentTweet(props) {
+  const { tweet } = props;
+  return tweet.parent ? (
+    <div className="row">
+      <div className="col-11 mx-auto p-3 border rounded">
+        <p className="mb-0 text-muted small">Retweet</p>
+        <Tweet className={' '} tweet={tweet.parent} />
+      </div>
+    </div>
+  ) : null;
+}
 export function Tweet(props) {
   const { tweet } = props;
   const className = props.className
@@ -116,9 +127,12 @@ export function Tweet(props) {
     : 'col-10 mx-auto col-md-6';
   return (
     <div className={className}>
-      <p>
-        {tweet.id} - {tweet.content}
-      </p>
+      <div>
+        <p>
+          {tweet.id} - {tweet.content}
+        </p>
+        <ParentTweet tweet={tweet} />
+      </div>
       <div className="btn btn-group">
         <ActionBtn tweet={tweet} action={{ type: 'like', display: 'Likes' }} />
         <ActionBtn
