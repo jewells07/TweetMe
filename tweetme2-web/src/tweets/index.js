99ae6192e3f704af24c97aae0553ff8743942f0e
jewells <jewellsjoshi437@gmail.com>
2020-08-09 12:54:22 +0530

    Feed View Component

--- tweetme2-web/src/tweets/index.js
+++ tweetme2-web/src/tweets/index.js
@@ -1,12 +1,17 @@
 import { ActionBtn } from './buttons';
 
-import { TweetsComponent, TweetDetailComponent } from './components';
+import {
+  FeedComponent,
+  TweetsComponent,
+  TweetDetailComponent,
+} from './components';
 import { TweetCreate } from './create';
 import { Tweet } from './detail';
 import { TweetsList } from './list';
 
 export {
   ActionBtn,
+  FeedComponent,
   Tweet,
   TweetCreate,
   TweetsComponent,
