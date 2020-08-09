e336d27e71b0f0e69a5fd157343efc00acf416fb
jewells <jewellsjoshi437@gmail.com>
2020-08-09 11:56:27 +0530

    Handling Pagination in React

--- tweetme2-web/src/tweets/lookup.js
+++ tweetme2-web/src/tweets/lookup.js
@@ -1,22 +1,26 @@
-import { backendLookup } from '../lookup';
+import {backendLookup} from '../lookup'
 
-export function apiTweetCreate(newTweet, callback) {
-  backendLookup('POST', '/tweets/create/', callback, { content: newTweet });
-}
+export function apiTweetCreate(newTweet, callback){
+    backendLookup("POST", "/tweets/create/", callback, {content: newTweet})
+  }
 
-export function apiTweetAction(tweetId, action, callback) {
-  const data = { id: tweetId, action: action };
-  backendLookup('POST', '/tweets/action/', callback, data);
+export function apiTweetAction(tweetId, action, callback){
+    const data = {id: tweetId, action: action}
+    backendLookup("POST", "/tweets/action/", callback, data)
 }
 
 export function apiTweetDetail(tweetId, callback) {
-  backendLookup('GET', `/tweets/${tweetId}/`, callback);
+    backendLookup("GET", `/tweets/${tweetId}/`, callback)
 }
 
-export function apiTweetList(username, callback) {
-  let endpoint = '/tweets/';
-  if (username) {
-    endpoint = `/tweets/?username=${username}`;
-  }
-  backendLookup('GET', endpoint, callback);
+
+export function apiTweetList(username, callback, nextUrl) {
+    let endpoint =  "/tweets/"
+    if (username){
+        endpoint =  `/tweets/?username=${username}`
+    }
+    if (nextUrl !== null && nextUrl !== undefined) {
+        endpoint = nextUrl.replace("http://localhost:8000/api", "")
+    }
+    backendLookup("GET", endpoint, callback)
 }
