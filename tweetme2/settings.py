7dd615f32eccdcbad01202c84d7e2be858ed0a88
jewells <jewellsjoshi437@gmail.com>
2020-08-09 13:27:19 +0530

    Build for the Feed

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -161,9 +161,9 @@ DEFAULT_AUTHENTICATION_CLASSES = [
 if DEBUG:
     DEFAULT_RENDERER_CLASSES += ['rest_framework.renderers.BrowsableAPIRenderer',]
 
-    DEFAULT_AUTHENTICATION_CLASSES += [
-        'tweetme2.rest_api.dev.DevAuthentication'
-    ]
+    # DEFAULT_AUTHENTICATION_CLASSES += [
+    #     'tweetme2.rest_api.dev.DevAuthentication'
+    # ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES' : DEFAULT_AUTHENTICATION_CLASSES,
