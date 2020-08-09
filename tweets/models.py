06fc01ef552a9ca827b6538ac067cf139abd0941
jewells <jewellsjoshi437@gmail.com>
2020-08-09 10:15:54 +0530

    ManyToManyField and Reverse Relations

--- tweets/models.py
+++ tweets/models.py
@@ -10,8 +10,10 @@ class TweetLike(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
 class Tweet(models.Model):
+    # Maps to SQL data
+    # id = models.AutoField(primary_key=True)
     parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
-    user = models.ForeignKey(User, on_delete=models.CASCADE) # Many users can many tweets
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tweets") # many users can many tweets
     likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
@@ -21,18 +23,18 @@ class Tweet(models.Model):
     #     return self.content
     
     class Meta:
-        ordering =['-id']
-
+        ordering = ['-id']
+    
     @property
     def is_retweet(self):
         return self.parent != None
-        
+    
     def serialize(self):
         '''
-        Feel Free to Delete !
+        Feel free to delete!
         '''
         return {
-            "id" : self.id,
-            "content" : self.content,
-            "likes" : random.randint(0, 200)
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0, 200)
         }
